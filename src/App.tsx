a24b48f8c065b06e5700e8400470ecc3635d4226
thesageak <sageaguinakang@gmail.com>
2025-10-13 05:46:54 -0700

    fixed README

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Background from './Background'
+import MainContent from './MainContent'
+
+function App() {
+  return (
+    <div>
+      <Background/>
+      <MainContent/>
+    </div>
+  )
+}
+
+export default App
